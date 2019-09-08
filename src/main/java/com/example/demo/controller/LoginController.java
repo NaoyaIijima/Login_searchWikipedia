af67dbb1ac80c6a77b7883ee80b112b73cf86827
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-08 12:42:33 +0000

    ログイン画面の完成

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController{
     @GetMapping("/login")
-    public getLogin(){
+    public String getLogin(){
         return "login";
     }
 }

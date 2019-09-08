e800208a9a2896e082f67100a75c61bffa9bb9bc
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-08 12:44:54 +0000

    データベースの作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO m_user (user_id, password)
+VALUES('yamada@xxx.co.jp', 'password');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS m_user (
+    user_id VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(100)
+);

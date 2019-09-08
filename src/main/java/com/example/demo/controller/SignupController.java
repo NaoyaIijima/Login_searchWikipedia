1f58685bff007061489a824087294e335a87933a
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-08 13:29:25 +0000

    form作った

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.form.SignupForm;
+import com.example.demo.service.SignupService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController{
     
     @Autowired
-    
+    SignupService signupService;
     
     @GetMapping("/signup")
     public String getMethod(){
@@ -18,11 +22,18 @@ public class SignupController{
     }
     
     @PostMapping("/signup")
-    public String postRequest(Model model){
+    public String postRequest(@ModelAttribute SignupForm form, Model model){
         
-        // データベースへ登録する処理
-        // 失敗した場合はsignup画面へ戻る
+        System.out.println(form);
+
         
+//        // データベースへ登録する処理
+//        // 失敗した場合はsignup画面へ戻る
+//        boolean result = signupService(user);
+//        
+//        if(result==false){
+//            return "signup";
+//        }
         
         return "redirect:/login";
     }
---
+++ src/main/java/com/example/demo/form/SignupForm.java
@@ -0,0 +1,9 @@
+package com.example.demo.form;
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+    private String userId; // ユーザーID
+    private String password; // パスワード
+}

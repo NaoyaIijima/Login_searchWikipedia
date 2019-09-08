f6ba937c5dd46b4504e172d1d27535e598b46d14
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-08 21:21:55 +0000

    データベース照合する前

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,18 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.form.SignupForm;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController{
     
+    @Autowired
+    SignupService signupService;
+    
     /**
      * ログイン画面のGETメソッド用処理.
      */
     @GetMapping("/login")
-    public String getLogin(Model model) {
+    public String getLogin(@ModelAttribute SignupForm form, Model model) {
 
         //login.htmlに画面遷移
         return "login";
@@ -22,8 +28,15 @@ public class LoginController{
      * ログイン画面のPOSTメソッド用処理.
      */
     @PostMapping("/login")
-    public String postLogin(Model model) {
+    public String postLogin(@ModelAttribute SignupForm form, Model model) {
 
+        System.out.println(form);
+        
+        // データベースに登録されているかを確認
+        boolean result = true;
+        
+        
+        
         //login.htmlに画面遷移
         return "redirect:/home";
     }

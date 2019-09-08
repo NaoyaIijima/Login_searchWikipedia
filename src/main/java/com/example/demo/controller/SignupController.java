e420e1b8cbbe90b892437c7da844dbb98247b608
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-09-08 20:49:05 +0000

    次はSQL文を書くところ

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.form.SignupForm;
 import com.example.demo.service.SignupService;
+import com.example.demo.repository.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,15 +26,19 @@ public class SignupController{
     public String postRequest(@ModelAttribute SignupForm form, Model model){
         
         System.out.println(form);
-
         
-//        // データベースへ登録する処理
-//        // 失敗した場合はsignup画面へ戻る
-//        boolean result = signupService(user);
-//        
-//        if(result==false){
-//            return "signup";
-//        }
+        User user = new User();
+        
+        user.setUserId(form.getUserId());
+        user.setPassword(form.getPassword());
+        
+        // データベースへ登録する処理
+        // 失敗した場合はsignup画面へ戻る
+        boolean result = signupService.isSignup(user);
+        
+        if(result==false){
+            return "signup";
+        }
         
         return "redirect:/login";
     }
--- src/main/java/com/example/demo/service/SignupService.java
+++ src/main/java/com/example/demo/service/SignupService.java
@@ -1,14 +1,26 @@
 package com.example.demo.service;
 
-import org.springframework.stereotype.Service;
-
 import com.example.demo.repository.User;
+import com.example.demo.repository.UserJdbcImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SignupService{
-    public boolean signup(User user){
+    @Autowired
+    UserJdbcImpl userJdbcImpl;
+    
+    public boolean isSignup(User user){        
         
         boolean result = true;
+        
+        int num = userJdbcImpl.insertOne(user);
+        
+        if(num<=0){
+            result = false;
+        }
+        
         return result;
     }
 }
